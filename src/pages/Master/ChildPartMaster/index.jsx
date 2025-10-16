7de0cd18b81d03c47826857b14dd5669c7ed251e
nandhiniSRT <157354684+snandhinisr@users.noreply.github.com>
2025-10-16 13:28:46 +0530

    childPartMaster ui integration

--- src/pages/Master/ChildPartMaster/index.jsx
+++ src/pages/Master/ChildPartMaster/index.jsx
@@ -8,6 +8,7 @@ import { SetFilterModule } from "ag-grid-enterprise";
 import { DateFilterModule } from "ag-grid-enterprise";
 import { toast } from "react-toastify";
 import serverApi from '../../../serverAPI';
+import store from "store";
 
 ModuleRegistry.registerModules([SetFilterModule, DateFilterModule]);
 
@@ -18,8 +19,9 @@ const ChildPartMaster = ({ modulesprop, screensprop }) => {
   const [originalList, setOriginalList] = useState([]);
   const gridRef = useRef(null);
 
-  const tenantId = JSON.parse(localStorage.getItem("tenantId"));
-  const branchCode = JSON.parse(localStorage.getItem('branchCode'));
+  const tenantId = store.get("tenantId")
+  const branchCode = store.get('branchCode');
+  const employeeId = store.get("employeeId")
 
   const autoSizeAllColumns = (params) => {
     if (!params.columnApi || !params.columnApi.getAllColumns) return;
@@ -30,37 +32,67 @@ const ChildPartMaster = ({ modulesprop, screensprop }) => {
   useEffect(() => {
     setSelectedModule(modulesprop);
     setSelectedScreen(screensprop);
+  }, [modulesprop, screensprop])
+
+  useEffect(() => {
     if (selectedModule && selectedScreen) {
-     fetchData()
+      fetchData();
     }
-  }, [selectedModule, selectedScreen]);
+  }, [selectedModule, selectedScreen])
 
-   const fetchData = async () => {
-      try {
-        const response = await serverApi.post("getchildpartMasterdtl", {
-          isActive: "1",
-          tenantId,
-          branchCode,
-        });
-        if (!response.data || response.data.length === 0) {
-          setMasterList([]);
-          setOriginalList([]);
-         
-        } else {
-          const updatedResponseData = response.data.map((item) => ({
-            ...item,
-            isUpdate: 1,
-          }));
-          setMasterList(updatedResponseData);
-          setOriginalList(updatedResponseData);
-          console.log(updatedResponseData);
-        }
-      } catch (error) {
-        console.error("Error fetching master data:", error);
-        toast.error("Error fetching data. Please try again later.");
+  const fetchData = async () => {
+    try {
+      const response = await serverApi.post("getchildpartMasterdtl", {
+        isActive: "1",
+        tenantId,
+        branchCode,
+      });
+      if (!response?.data?.responseCode === '200') {
+        setMasterList([]);
+        setOriginalList([]);
+        toast.error(response.data.responseMessage)
+      } else {
+        console.log(response)
+        const updatedResponseData = response?.data?.responseData.map((item) => ({
+          ...item,
+          isUpdate: 1,
+        }));
+        setMasterList(updatedResponseData);
+        setOriginalList(updatedResponseData);
       }
-    };
+    } catch (error) {
+      console.error("Error fetching master data:", error);
+      toast.error("Error fetching data. Please try again later.");
+    }
+  };
 
+  const createorUpdate = async () => {
+    try {
+      const updatedList = masterList.map(item => ({
+        isUpdate: item.isUpdate,
+        childPartCode: item.childPartCode,
+        childPartDesc: item.childPartDesc,
+        product: item.product,
+        line: item.line,
+        tenantId,
+        status: item.status,
+        updatedBy: employeeId,
+        branchCode,
+      }));
+
+      const response = await serverApi.post("insertupdatechildpartmaster", updatedList);
+
+      if (response?.responseCode === '200') {
+        toast.success(response.data.responseMessage)
+        fetchData();
+      } else {
+        toast.error(response.data.responseMessage)
+      }
+    } catch (error) {
+      console.error("Error saving data:", error);
+      toast.error("Error while saving data!");
+    }
+  }
   const defaultColDef = {
     sortable: true,
     filter: true,
@@ -69,7 +101,7 @@ const ChildPartMaster = ({ modulesprop, screensprop }) => {
   };
 
   const columnDefs = [
-    { headerName: "Id", field: "id", filter: "agNumberColumnFilter", editable: false },
+    // { headerName: "Id", field: "id", filter: "agNumberColumnFilter", editable: false },
     { headerName: "Child Part Code", field: "childPartCode", filter: "agTextColumnFilter" },
     { headerName: "Child Part Desc", field: "childPartDesc", filter: "agTextColumnFilter" },
     { headerName: "Product", field: "product", filter: "agTextColumnFilter" },
@@ -81,9 +113,10 @@ const ChildPartMaster = ({ modulesprop, screensprop }) => {
       editable: true,
       cellRenderer: "agCheckboxCellRenderer",
       cellEditor: "agCheckboxCellEditor",
-      valueGetter: (params) => params.data.status === true,
+      valueGetter: (params) => params.data.status === "1" || params.data.status === 1,
       valueSetter: (params) => {
-        params.data.status = params.newValue ? true : false;
+        // when checkbox is clicked, set 1 for true, 0 for false
+        params.data.status = params.newValue ? "1" : "0";
         return true;
       },
       cellStyle: { textAlign: "center" },
@@ -92,21 +125,26 @@ const ChildPartMaster = ({ modulesprop, screensprop }) => {
 
   // Add new empty row
   const handleAddRow = () => {
-     const nextId =
-    masterList.length > 0
-      ? Math.max(...masterList.map((item) => Number(item.id) || 0)) + 1
-      : 1;
-
-  // Create an empty row with that ID
-  const emptyRow = { id: nextId };
-  columnDefs.forEach((col) => {
-    if (col.field !== "id") {
-      emptyRow[col.field] = "";
-    }
-  });
-    const updated = [...masterList, emptyRow];
-    setMasterList(updated);
-    setOriginalList(updated);
+    const emptyRow = {
+        isUpdate:0
+      };
+      const childPartCodeEmpty = masterList.filter((item)=> !item.childPartCode);
+  
+        if(childPartCodeEmpty && childPartCodeEmpty?.length === 0){
+          const updated = [...masterList, emptyRow];
+          setMasterList(updated);
+          setOriginalList(updated);
+        }else{
+        // ("Please enter the empty rows.");
+        toast.error("Please enter the empty rows.");
+        }
+    // const emptyRow = { id: nextId };
+    // columnDefs.forEach((col) => {
+    //   if (col.field !== "id") {
+    //     emptyRow[col.field] = "";
+    //   }
+    // });
+    
   };
 
   // Cancel
@@ -115,6 +153,7 @@ const ChildPartMaster = ({ modulesprop, screensprop }) => {
     setSelectedScreen("");
     setMasterList([]);
     setOriginalList([]);
+    fetchData()
   };
 
   // Filter change
@@ -139,80 +178,79 @@ const ChildPartMaster = ({ modulesprop, screensprop }) => {
   };
 
   return (
-    <div className="container mt-1">
-      {masterList.length > 0 && (
-        <div className="card shadow mt-4" style={{ borderRadius: "6px" }}>
-          <div
-            className="card-header text-white fw-bold d-flex justify-content-between align-items-center"
-            style={{ backgroundColor: "#00264d" }}
-          >
-            {selectedScreen} Details
-            <PlusOutlined
-              style={{ fontSize: "20px", cursor: "pointer", color: "white" }}
-              onClick={handleAddRow}
-              title="Add Row"
-            />
-          </div>
+    <div className="container mt-1 p-0">
+      <div className="card shadow mt-4" style={{ borderRadius: "6px" }}>
+        <div
+          className="card-header text-white fw-bold d-flex justify-content-between align-items-center"
+          style={{ backgroundColor: "#00264d" }}
+        >
+          {selectedScreen} Details
+          <PlusOutlined
+            style={{ fontSize: "20px", cursor: "pointer", color: "white" }}
+            onClick={handleAddRow}
+            title="Add Row"
+          />
+        </div>
 
-          {/* Filter Dropdown */}
-          <div className="p-3">
-            <div className="row">
-              <div className="col-md-3">
-                <label className="form-label fw-bold">Search Filter</label>
-                <select className="form-select" onChange={(e) => handleFilterChange(e.target.value)}>
-                  <option value="GetAll">Get All</option>
-                  <option value="Active">Active</option>
-                  <option value="Inactive">Inactive</option>
-                </select>
-              </div>
-            </div>
-          </div>
-
-          <div className="card-body p-3">
-            <AgGridReact
-              ref={gridRef}
-              rowData={masterList}
-              columnDefs={columnDefs}
-              defaultColDef={defaultColDef}
-              paginationPageSize={100}
-              pagination={true}
-              domLayout="autoHeight"
-              singleClickEdit={true}
-              onFirstDataRendered={autoSizeAllColumns}
-              onCellValueChanged={(params) => {
-                const updatedList = [...masterList];
-                updatedList[params.rowIndex] = params.data;
-                setMasterList(updatedList);
-                setOriginalList(updatedList);
-              }}
-            />
-            <div className="text-center mt-4">
-              <button
-                onClick={() => onExportExcel(gridRef)}
-                className="btn text-white me-2"
-                style={{ backgroundColor: "#00264d", minWidth: "90px" }}
-              >
-                Excel
-              </button>
-              <button
-                type="submit"
-                className="btn text-white me-2"
-                style={{ backgroundColor: "#00264d", minWidth: "90px" }}
-              >
-                Update
-              </button>
-              <button
-                type="button"
-                onClick={handleCancel}
-                className="btn text-white"
-                style={{ backgroundColor: "#00264d", minWidth: "90px" }}
-              >
-                Cancel
-              </button>
+        {/* Filter Dropdown */}
+        <div className="p-3">
+          <div className="row">
+            <div className="col-md-3">
+              <label className="form-label fw-bold">Search Filter</label>
+              <select className="form-select" onChange={(e) => handleFilterChange(e.target.value)}>
+                <option value="GetAll">Get All</option>
+                <option value="Active">Active</option>
+                <option value="Inactive">Inactive</option>
+              </select>
             </div>
           </div>
         </div>
-      )}
+
+        <div className="card-body p-3">
+          <AgGridReact
+            ref={gridRef}
+            rowData={masterList}
+            columnDefs={columnDefs}
+            defaultColDef={defaultColDef}
+            paginationPageSize={100}
+            pagination={true}
+            domLayout="autoHeight"
+            singleClickEdit={true}
+            onFirstDataRendered={autoSizeAllColumns}
+            onCellValueChanged={(params) => {
+              const updatedList = [...masterList];
+              updatedList[params.rowIndex] = params.data;
+              setMasterList(updatedList);
+              setOriginalList(updatedList);
+            }}
+          />
+          <div className="text-center mt-4">
+            <button
+              onClick={() => onExportExcel(gridRef)}
+              className="btn text-white me-2"
+              style={{ backgroundColor: "#00264d", minWidth: "90px" }}
+            >
+              Excel
+            </button>
+            <button
+              type="submit"
+              className="btn text-white me-2"
+              style={{ backgroundColor: "#00264d", minWidth: "90px" }}
+              onClick={createorUpdate}
+            >
+              Update
+            </button>
+            <button
+              type="button"
+              onClick={handleCancel}
+              className="btn text-white"
+              style={{ backgroundColor: "#00264d", minWidth: "90px" }}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
